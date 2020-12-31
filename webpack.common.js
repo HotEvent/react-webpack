817f21d73dbe32b76c73f92a290b700254c67473
HotEvent <1807657825@qq.com>
2020-12-31 22:12:18 +0800

    update

--- webpack.common.js
+++ webpack.common.js
@@ -22,9 +22,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+                type: 'asset/resource'
             }
         ]
     },
