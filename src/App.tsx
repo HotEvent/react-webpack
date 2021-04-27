2a70d0032efce3c60e8a22353a33cbcfbf739424
HotEvent <1807657825@qq.com>
2021-04-27 19:58:00 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,34 @@ import '@/styles/style.scss';
 import { AppState } from '@/reducers';
 import { Login } from '@/layouts/Login';
 import { Main } from '@/layouts/Main';
+import { useState } from '.';
 
 export const App: React.FC = () => {
-  const dispatch = useDispatch();
-  const table = useSelector<AppState, { name: string }[]>(
-    (state) => state.table,
-  );
-  useEffect(() => {
-    dispatch({ type: 'table_start' });
-  }, []);
+  // const dispatch = useDispatch();
+  // const table = useSelector<AppState, { name: string }[]>(
+  //   (state) => state.table,
+  // );
+  // useEffect(() => {
+  //   dispatch({ type: 'table_start' });
+  // }, []);
+  const [age, setAge] = useState('foo');
   return (
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/app">
-          <Main />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <div>{age}</div>
+      <button
+        onClick={(e) => {
+          setAge(2);
+        }}
+      >
+        update
+      </button>
+      <button
+        onClick={(e) => {
+          // setAge((age) => age + 1);
+        }}
+      >
+        update2
+      </button>
+    </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,92 @@ import { epicMiddleware, rootEpic } from './reducers';
 import configureStore from './configureStore';
 const store = configureStore();
 epicMiddleware.run(rootEpic);
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+
+const root = {
+  current: null,
+  workInProgress: null,
+  dom: null,
+  hook: null,
+};
+
+const findEventHandle = (element) => {
+  if (element?.props.onClick) {
+    element.props.onClick();
+  } else {
+    if (element.props.children) {
+      if (Array.isArray(element.props.children)) {
+        for (const c of element.props.children) {
+          findEventHandle(c);
+        }
+      } else if (typeof element.props.children === 'object') {
+        findEventHandle(element.props.children);
+      }
+    }
+  }
+};
+
+const MRender = (
+  element: React.ReactElement<any, any>,
+  domElement: HTMLElement,
+) => {
+  domElement.addEventListener('click', (e) => {
+    findEventHandle(root.current);
+  });
+
+  root.dom = domElement;
+  const { type, props } = element;
+  const tree = type(props);
+  root.workInProgress = tree;
+  effectTree(tree, domElement);
+  root.current = tree;
+};
+
+export const useState = (action: any) => {
+  root.hook = { state: action };
+  const dispatchAction = (action) => {
+    root.hook.state = action;
+    console.log('update');
+  };
+  return [root.hook.state, dispatchAction];
+};
+
+const effectTree = (
+  element: React.ReactElement<any, any>,
+  domElement: HTMLElement,
+) => {
+  const { type, props } = element;
+  if (typeof type === 'function') {
+    const tree = element.type(props);
+    effectTree(tree, domElement);
+  } else if (typeof type === 'string') {
+    const delement = document.createElement(type);
+    domElement.appendChild(delement);
+    const children = props.children;
+    if (Array.isArray(children)) {
+      for (const c of props.children) {
+        effectTree(c, delement);
+      }
+    } else if (typeof children === 'string') {
+      delement.textContent = children;
+    } else if (typeof children === 'object') {
+      effectTree(children, delement);
+    }
+  }
+};
+
+MRender(
+  // <Provider store={store}>
+  <App />,
+  // </Provider>
   document.getElementById('root'),
 );
-if (module.hot) {
-  module.hot.accept('./App', function () {
-    ReactDOM.render(
-      <Provider store={store}>
-        <App />
-      </Provider>,
-      document.getElementById('root'),
-    );
-  });
-}
+// if (module.hot) {
+//   module.hot.accept('./App', function () {
+//     MRender(
+//       // <Provider store={store}>
+//       <App />,
+//       // </Provider>
+//       document.getElementById('root'),
+//     );
+//   });
+// }
